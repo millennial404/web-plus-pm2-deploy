30cc09ed3901ee761e697802512b23ceb8965a22
s.zaharchenko <s.zaharchenko@smrtc.ru>
2024-08-06 16:58:57 +0300

    pm2 customization

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,21 +3,26 @@ import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
-import { errors } from 'celebrate';
-// import cors from 'cors';
+import {errors} from 'celebrate';
+import cors from 'cors';
 import errorHandler from './middlewares/error-handler';
-import { DB_ADDRESS } from './config';
+import {DB_ADDRESS} from './config';
 import routes from './routes';
 
-const { PORT = 3000 } = process.env;
+const {PORT = 3000} = process.env;
 const app = express();
 mongoose.connect(DB_ADDRESS);
 
 // Только для локальных тестов. Не используйте это в продакшене
-// app.use(cors())
+app.use(cors())
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.use(routes);
 app.use(errors());
 app.use(errorHandler);
