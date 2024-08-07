7634e0954e8dca55394f2e8d346069c742f7b00b
s.zaharchenko <s.zaharchenko@smrtc.ru>
2024-08-07 16:52:25 +0300

    fix ecosystem.config.js

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      'post-deploy': 'cd frontend && pwd && npm ci && npm install caniuse-lite && npm run build',
+      'post-deploy': 'cd frontend && pwd && npm install && npm install -i && npm install caniuse-lite && npm run build',
     },
   },
 };
